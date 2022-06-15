d4709ed46a08dc9e74e9739b554b988704d10885
u-kai <76635578+u-kai@users.noreply.github.com>
2022-06-15 22:17:46 +0900

    add is_individial

--- src/nws/macaddr.rs
+++ src/nws/macaddr.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, ops::Deref};
 
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct MacAddr([u8; 6]);
@@ -6,13 +6,45 @@ impl MacAddr {
     pub fn new(addr: [u8; 6]) -> Self {
         MacAddr(addr)
     }
+    pub fn is_individual(&self) -> bool {
+        let head = self[0];
+        (head << 7) == 0
+    }
+    pub fn is_group(&self) -> bool {
+        self[0] == 1
+    }
+    pub fn is_global(&self) -> bool {
+        //self[]
+        true
+    }
 }
 impl Display for MacAddr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "{:^02x}-{:^02x}-{:^02x}-{:^02x}-{:^02x}-{:^02x}",
-            self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5],
+            self[0], self[1], self[2], self[3], self[4], self[5],
         )
+    }
+}
+
+impl Deref for MacAddr {
+    type Target = [u8; 6];
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+#[cfg(test)]
+mod mac_addr_test {
+    use super::MacAddr;
+
+    #[test]
+    fn is_individual_test() {
+        let mac = MacAddr::new([0, 1, 1, 1, 1, 1]);
+        assert!(mac.is_individual());
+        let mac = MacAddr::new([254, 1, 1, 1, 1, 1]);
+        assert!(mac.is_individual());
+        let mac = MacAddr::new([1, 1, 1, 1, 1, 1]);
+        assert!(!mac.is_individual());
     }
 }
