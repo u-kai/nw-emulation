d89f0c92833e54cb6ffac2babef42f36ae53e709
u-kai <76635578+u-kai@users.noreply.github.com>
2022-06-20 20:59:53 +0900

    add tls trait

--- src/security/tls.rs
+++ src/security/tls.rs
@@ -1,1 +1,46 @@
+pub trait TlsClient {
+    fn client_hello(&self) -> (CryptoAlgorisumParameter, ClientRandom);
+    fn valid_server_certificate(&self, cert: ServerCertificate) -> bool;
+    fn certificate(&self) -> ClientCertificate;
+    fn client_key_exchange(&mut self) -> PreMasterSecret;
+    fn client_verify(&self) -> DigitalSignature;
+    fn change_cipher_spec(&mut self) -> MasterSecret;
+    fn finished(&self) -> ValidateData;
+    fn valid_data(&self, data: ValidateData) -> bool;
+}
 
+struct ValidateData;
+struct MasterSecret;
+struct DigitalSignature;
+struct PreMasterSecret;
+
+struct ClientCertificate {
+    cert: String,
+}
+struct CryptoAlgorisumParameter {
+    protocol_v: String,
+    crypto_suite: CryptoSuite,
+}
+struct ClientRandom;
+
+struct CryptoSuite {
+    server_cert_algo: String,
+    key_exchange_algo: String,
+    messeage_crypto_algo: String,
+    use_mode: String,
+    hash_algo: String,
+}
+pub trait TlsServer {
+    fn server_hello(&self) -> (CryptoAlgorisumParameter, ServerRandom);
+    fn certificate(&self) -> ServerCertificate;
+    fn certificate_request(&self) -> ();
+    fn server_done(&self) -> ();
+    fn valid_client_signater(&self, signature: DigitalSignature) -> bool;
+    fn change_cipher_spec(&mut self) -> ();
+    fn finished(&self) -> ValidateData;
+}
+struct ServerRandom(u32);
+
+struct ServerCertificate {
+    cert: String,
+}
